113a52284173c255f9b7762ce94d6854ab7af0e2
joy2362 <abdullahzahidjoy@gmail.com>
2023-06-26 19:24:26 +0600

    search api integration

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,8 @@
 <template>
   <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <a class="navbar-brand" href="/">Super Store</a>
+    <router-link :to="{ path:'/' }">
+      <a class="navbar-brand">Super Store</a>
+    </router-link>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
@@ -8,7 +10,7 @@
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <form @submit.prevent="search" class="form-inline my-2 my-lg-0">
         <input class="form-control mr-sm-2" type="search" placeholder="Search"
-               v-model="keyword"  aria-label="Search">
+               aria-label="Search" v-model="keyword">
         <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
       </form>
     </div>
@@ -16,15 +18,19 @@
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   data(){
     return{
-      keyword:''
+      keyword: ''
     }
   },
   methods:{
     search(){
-      this.$emit('search', this.keyword)
+      axios.get('http://api_server.test/api/data?search=' + this.keyword).then(response => {
+        console.log(response.data)
+      })
     }
   }
 }
